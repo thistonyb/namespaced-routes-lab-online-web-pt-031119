77ed58e214394628e6f7be7a3e3024ca0b9710e5
Tony Baker <this.tonybaker@gmail.com>
2019-08-13 23:57:42 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
+
   def index
-    @artists = Artist.all
+    @artists = Artist.all.order("name " + preference.artist_sort_order)
   end
 
   def show
@@ -8,7 +9,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if preference.allow_create_artists
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
