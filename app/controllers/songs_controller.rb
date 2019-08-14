b026a6726ac69578c9b18751d3cb23aea5c7368e
Tony Baker <this.tonybaker@gmail.com>
2019-08-14 00:22:42 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,15 @@
 class SongsController < ApplicationController
+
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
       else
-        @songs = @artist.songs
+        @songs = @artist.songs.order("title " + preference.song_sort_order)
       end
     else
-      @songs = Song.all
+      @songs = Song.all.order("title " + preference.song_sort_order)
     end
   end
 
@@ -25,7 +26,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if preference.allow_create_songs
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
   end
 
   def create
@@ -67,4 +72,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
